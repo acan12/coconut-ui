4e599737d13e2837a5ed43b7e02740a4c3bfeb5e
ary <ary@clappingape.com>
2018-11-13 15:17:37 +0700

    add components

---
+++ coconutui-base/src/main/java/app/coconut/ui/com/beelabs/component/AccordionContentComponent.java
@@ -0,0 +1,106 @@
+package app.coconut.ui.com.beelabs.component;
+
+import android.animation.ValueAnimator;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.LinearLayout;
+
+public class AccordionContentComponent {
+
+    public static class AccordionCallback {
+        public void onExpandListener() {
+
+        }
+
+        public void onCollapseListener() {
+
+        }
+    }
+
+
+    public static void accordionView(View viewButton, final View viewContent, boolean defaultExpand, final AccordionCallback callback) {
+        int contentHeight = ((LinearLayout.LayoutParams) viewContent.getLayoutParams()).height;
+        accordionView(viewButton, viewContent, contentHeight, defaultExpand, callback);
+    }
+
+    public static void accordionView(View viewButton, final View viewContent, final int contentHeight, boolean defaultExpand, final AccordionCallback callback) {
+
+        if (defaultExpand) {
+            expandAccordion(viewContent, contentHeight, callback);
+        } else {
+            collapseAccordion(viewContent, contentHeight, callback);
+        }
+
+        viewButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                boolean expand = viewContent.getVisibility() == View.GONE;
+                if (expand) {
+                    expandAccordion(viewContent, contentHeight, callback);
+                } else {
+                    collapseAccordion(viewContent, contentHeight, callback);
+                }
+            }
+        });
+
+
+    }
+
+    public static void collapseAccordion(final View content, final int contentHeight, final AccordionCallback callback) {
+
+        content.post(new Runnable() {
+            @Override
+            public void run() {
+                int initialHeight = contentHeight;//content.getMeasuredHeight();
+
+                ValueAnimator valueAnimator = ValueAnimator.ofInt(initialHeight, 0);
+                valueAnimator.setInterpolator(new DecelerateInterpolator());
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        content.getLayoutParams().height = (int) animation.getAnimatedValue();
+                        content.requestLayout();
+                        if ((int) animation.getAnimatedValue() == 0)
+                            content.setTag(content.getMeasuredHeight());
+                        content.setVisibility(View.GONE);
+
+
+                        if (callback != null) callback.onCollapseListener();
+                    }
+                });
+                valueAnimator.setInterpolator(new DecelerateInterpolator());
+                valueAnimator.setDuration(500);
+                valueAnimator.start();
+            }
+        });
+
+
+    }
+
+    public static void expandAccordion(final View content, final int contentHeight, final AccordionCallback callback) {
+
+        content.post(new Runnable() {
+            @Override
+            public void run() {
+
+                int targetHeight = contentHeight;//content.getMeasuredHeight();
+                content.setVisibility(View.VISIBLE);
+                ValueAnimator valueAnimator = ValueAnimator.ofInt(targetHeight);
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        content.getLayoutParams().height = (int) animation.getAnimatedValue();
+                        content.requestLayout();
+                        if (callback != null) callback.onExpandListener();
+                    }
+                });
+                valueAnimator.setInterpolator(new DecelerateInterpolator());
+                valueAnimator.setDuration(500);
+                valueAnimator.start();
+            }
+        });
+
+
+    }
+}
---
+++ coconutui-base/src/main/java/app/coconut/ui/com/beelabs/component/SpinnerComponent.java
@@ -0,0 +1,109 @@
+package app.coconut.ui.com.beelabs.component;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class SpinnerComponent {
+
+    public static void show(int rowSpinnerLayoutId, final int rowSpinnerId, List<String> dataLabel, final Spinner spinner, final TextView target, Context context) {
+        show(rowSpinnerLayoutId, rowSpinnerId, dataLabel, spinner, target, context, null);
+    }
+
+    public static void show(int rowSpinnerLayoutId, final int rowSpinnerId, List<String> dataLabel, final Spinner spinner, final TextView target, Context context, final ItemSelectedCallback callback) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, rowSpinnerLayoutId, rowSpinnerId,
+                dataLabel);
+        spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView spinnerView = view.findViewById(rowSpinnerId);
+                String res = spinnerView.getText().toString();
+                target.setText(res);
+
+                if (callback != null) callback.onCall(res);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        target.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner.performClick();
+            }
+        });
+    }
+
+    public static void showWithCustomId(int rowSpinnerLayoutId, final int rowSpinnerId, List<String> dataLabel, final List<Integer> dataValue, final Spinner spinner, final TextView target, Context context, final ItemSelectedCallback callback) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, rowSpinnerLayoutId, rowSpinnerId,
+                dataLabel);
+        spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView spinnerView = view.findViewById(rowSpinnerId);
+                String res = spinnerView.getText().toString();
+                target.setText(res);
+
+                if (callback != null) callback.onCall(res, dataValue.get(position));
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        target.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner.performClick();
+            }
+        });
+    }
+
+
+    public static void show(int rowSpinnerLayoutId, final int rowSpinnerId, final List<String> dataName, final List<int[]> dataValue, final Spinner spinner, final TextView target, Context context) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, rowSpinnerLayoutId, rowSpinnerId,
+                dataName);
+        spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView spinnerView = view.findViewById(rowSpinnerId);
+                String res = spinnerView.getText().toString();
+                target.setText(res);
+                target.setTag(dataValue.get(0)[position]);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        target.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner.performClick();
+            }
+        });
+    }
+
+
+    public static class ItemSelectedCallback {
+        public void onCall(String name) {
+
+        }
+
+        public void onCall(String name, Object value) {
+
+        }
+    }
+}
