5e5fd22dfd22477551423417cf0c6075517c205c
ary <ary@clappingape.com>
2020-06-22 14:04:52 +0700

    update component listener for dropdown

--- coconutui-base/src/main/java/app/coconut/ui/com/beelabs/ui/slide/UiDropdownOnBottom.kt
+++ coconutui-base/src/main/java/app/coconut/ui/com/beelabs/ui/slide/UiDropdownOnBottom.kt
@@ -10,80 +10,75 @@ import app.coconut.ui.com.beelabs.R
 
 
 class UiDropdownOnBottom {
-
-    class UiDropDown(var view: View)
+    var panelSlide: View? = null
+    var parent: ViewGroup? = null
 
     companion object {
-        fun build(
-            data: List<DropDownItemModel>,
-            parentLayout: ViewGroup,
-            slideLayoutResource: Int,
-            slideContentID: Int,
-            context: Context,
-            itemListener: OnItemListener
-        ): View? {
+        fun newInstance(): UiDropdownOnBottom {
+            return UiDropdownOnBottom()
+        }
+    }
 
-            val inflater =
-                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            var panelSlide: View? = inflater.inflate(
-                slideLayoutResource,
-                parentLayout,
-                false
-            ) as View
-            parentLayout.addView(panelSlide)
-            val panelBg = panelSlide!!.findViewById<View>(R.id.slideBackground)
-            val panelContent = panelSlide!!.findViewById<ViewGroup>(slideContentID)
+    fun build(
+        data: List<DropDownItemModel>,
+        parentLayout: ViewGroup,
+        slideLayoutResource: Int,
+        slideContentID: Int,
+        context: Context,
+        itemListener: OnItemListener
+    ): View? {
 
-            for((i, item) in data.withIndex()){
-                var itemView = LayoutInflater.from(context).inflate(R.layout.item_dropdown, panelContent, false)
-                itemView.findViewById<TextView>(R.id.tv_item).text = item.name
-                itemView.findViewById<TextView>(R.id.tv_item).setOnClickListener { itemListener.onClick(itemView, i) }
-                panelContent.addView(itemView)
-            }
+        val inflater =
+            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var panelSlide = inflater.inflate(
+            slideLayoutResource,
+            parentLayout,
+            false
+        ) as View
+        this.panelSlide = panelSlide
+        this.parent = parentLayout
 
-            // buttons panel
-            val bottomUp = AnimationUtils.loadAnimation(context, R.anim.slide_up)
-            panelContent.startAnimation(bottomUp)
-            panelContent.visibility = View.VISIBLE
-//            var dropDown = UiDropDown(panelSlide)
-            return panelSlide
+        parentLayout.addView(panelSlide)
+        val panelBg = panelSlide!!.findViewById<View>(R.id.slideBackground)
+        val panelContent = panelSlide!!.findViewById<ViewGroup>(slideContentID)
+
+        for ((i, item) in data.withIndex()) {
+            var itemView =
+                LayoutInflater.from(context).inflate(R.layout.item_dropdown, panelContent, false)
+            itemView.findViewById<TextView>(R.id.tv_item).text = item.name
+            itemView.findViewById<TextView>(R.id.tv_item)
+                .setOnClickListener { itemListener.onClick(itemView, i) }
+            panelContent.addView(itemView)
+        }
+        panelBg.setOnClickListener { itemListener.outItemClick() }
+        // buttons panel
+        val bottomUp = AnimationUtils.loadAnimation(context, R.anim.slide_up)
+        panelContent.startAnimation(bottomUp)
+        panelContent.visibility = View.VISIBLE
+
+        return panelSlide
+    }
+
+    fun dismiss(): Boolean {
+        return try {
+            if (panelSlide != null) {
+                parent!!.removeView(panelSlide)
+                this.panelSlide = null
+                true
+            } else false
+        } catch (e: Exception) {
+            false
         }
 
-        private fun onItemClickListener(itemView: View?, i: Int) {
-
-        }
-
-//    fun showTopPanel(context: Context, attachedRoot: ViewGroup, resourcePanelID: Int, onAttachedDataSlidePanel: app.clappingape.com.morinaga.ui.component.SlidePanel.OnAttachedDataSlidePanel?): View? {
-//        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-//        val panelSlide = inflater.inflate(resourcePanelID, attachedRoot, false) as ViewGroup // (View) root.findViewById(R.id.panel_background_slide);
-//        if (onAttachedDataSlidePanel != null) {
-//            onAttachedDataSlidePanel.onBindDataPanel(panelSlide)
-//        } else {
-//            attachedRoot.addView(panelSlide)
-//        }
-//        val panelContent = panelSlide.findViewById<ViewGroup>(R.id.panel_content)
-//        // buttons panel
-//        val topDown = AnimationUtils.loadAnimation(context, R.anim.slide_down)
-//        panelContent.startAnimation(topDown)
-//        panelContent.visibility = View.VISIBLE
-//        return panelSlide
-//    }
     }
 
     open class OnItemListener {
-        open fun onClick(view: View, pos: Int){
+        open fun onClick(view: View, pos: Int) {
 
         }
+
+        open fun outItemClick(){}
     }
 }
 
-class DropDownItemModel {
-
-    var id: Int = 0
-    var name: String? = ""
-
-    constructor(id: Int, name: String) {
-        this.id = id
-        this.name = name
-    }
-}
+class DropDownItemModel(var name: String)
